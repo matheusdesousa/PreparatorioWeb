0351a094a22b5b0172f2ee89e6da8df087d9cbc7
Matheus Lima <matheusdslhc@gmail.com>
2019-10-20 23:47:31 -0300

    Add ModuloI-Q5

---
+++ ModuloI-Q5.js
@@ -0,0 +1,17 @@
+const aluno = {
+    nome:'Iago',
+    notas: [
+        {nome: 'Física', valor: 5},
+        {nome: 'Estatística', valor: 4},
+        {nome: 'Probabilidade', valor: 1}
+    ]
+}
+
+function imprimirNota(nota) {
+    console.log(nota); 
+}
+
+
+console.log(aluno.notas[0].valor)
+console.log(aluno.notas[0].valor)
+console.log(aluno.notas[0].valor)
