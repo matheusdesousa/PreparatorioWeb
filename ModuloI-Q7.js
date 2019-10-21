0c1119ff1252b45c7f12ad9d8fc2e40712678f35
Matheus Lima <matheusdslhc@gmail.com>
2019-10-21 01:04:07 -0300

    Add ModuloI-Q7

---
+++ ModuloI-Q7.js
@@ -0,0 +1,11 @@
+function try_catch(){
+    try{
+        if (Math.random() > 0.3)
+            throw new Error()
+            return
+    }catch(e){
+
+    }finally{
+        console.log("Esta mensagem sempre será exibida")
+    }
+}
